598e7e9313c7c3081f92f774205503c5eb816fa3
hanif32 <140376744+hanif32@users.noreply.github.com>
2024-05-19 00:03:13 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,25 +1,43 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Perpus Online</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <title></title>
+    <style>
+        .custom-form {
+            box-shadow: 4px 4px 5px -4px;
+            padding: 10px;
+        }
+    </style>
 </head>
 <body>
-    <div class="row" style="margin-top:50px;">
-    <div class="col-md"></div>
-    <div class="col-md rounded bg-light" style="box-shadow: 4px 4px 5px -4px;padding:10px">
-        <form action="proses_login.php" method="post">
-          <h3 align="center">LOGIN Perpus Online</h3>
-          Username:
-          <input type="text" name="username" value="" class="form-control">
-          password:
-          <input type="password" name="password" class="form-control"><br>
-          <center><input type="submit" name="login" class="btn btn-success" value="LOGIN"></center>
-      </form>
+    <div class="container">
+        <div class="row justify-content-md-center mt-5">
+            <div class="col-md-4">
+                <div class="rounded bg-light custom-form">
+                    <form action="proses_login.php" method="post">
+                        <h3 class="text-center">LOGIN Perpus Online</h3>
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Username:</label>
+                            <input type="text" id="username" name="username" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password:</label>
+                            <input type="password" id="password" name="password" class="form-control">
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" name="login" class="btn btn-success">LOGIN</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-md"></div>
-  </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
+
 
